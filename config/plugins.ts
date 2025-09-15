e318204686c5d3d17f9437ddfff00384071dff2d
Bruno Cukic <>
2025-09-15 12:23:39 +0200

    Initial Strapi setup with AWS S3 configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => {    
+    return {
+      upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            s3Options: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+              region: env('AWS_REGION'),
+              params: {
+                Bucket: env('AWS_BUCKET'),
+                ACL: 'public-read',
+              },
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
+    };
+  };
